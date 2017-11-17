b1f88de9c1d96e1ff6ce1bf70c1b57918c486823
Jiang Fengming <fenivana@noindoin.com>
2017-11-17 21:52:35 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+async function loadPage(url, { userAgent }) {
+
+}
+
+async function getPageTitle(url, opts) {
+
+}
+
+async function getPageContent(url, opts) {
+
+}
+
+async function getPageScreenshot(url, opts) {
+
+}
